f2e8dd54ebe2b6ca6f098e590406eb016ee64ce9
Felipe Andrade <feandrad@gmail.com>
2020-02-05 11:08:38 -0300

    Small changes

--- data/src/main/java/characters/responses/Data.kt
+++
@@ -1,7 +0,0 @@
-package characters.responses
-
-data class Data(val total: Int = 0,
-                val offset: Int = 0,
-                val limit: Int = 0,
-                val count: Int = 0,
-                val results: List<ResultsItem>?)
