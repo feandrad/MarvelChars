6e2ec68f1292497efb660f0426e146a7f1e29f00
Felipe Andrade <feandrad@gmail.com>
2020-02-05 03:49:56 -0300

    api call success

--- data/src/main/java/CharacterRepository.kt
+++ data/src/main/java/CharacterRepository.kt
@@ -2,12 +2,80 @@ package io.felipeandrade.marvelchars.data
 
 import io.felipeandrade.marvelchars.data.characters.CharacterApi
 import io.felipeandrade.marvelchars.domain.MarvelCharacter
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 
 class CharacterRepository(private val characterApi: CharacterApi) {
 
-    suspend fun loadAll(): List<MarvelCharacter> = characterApi.loadAllCharacters()
+    suspend fun loadAll(): List<MarvelCharacter> {
+        val ts = System.currentTimeMillis().toString()
+        return characterApi.loadAllCharacters(
+            ts, BuildConfig.PUBLIC_API_KEY, getMd5(ts)
+        )
+    }
 
-    suspend fun loadCharacter(charId: Int): MarvelCharacter = characterApi.loadCharacterById(charId)
+    suspend fun loadCharacter(charId: Int): MarvelCharacter {
+        val ts = System.currentTimeMillis().toString()
+        return characterApi.loadCharacterById(
+            ts, BuildConfig.PUBLIC_API_KEY, getMd5(ts), charId
+        )
+    }
+
+    private fun getMd5(ts: String): String {
+        try {
+
+            val md = MessageDigest.getInstance("MD5")
+
+            val messageDigest = md.digest(
+                ts.toByteArray()
+                        + BuildConfig.PRIVATE_API_KEY.toByteArray()
+                        + BuildConfig.PUBLIC_API_KEY.toByteArray()
+            )
+
+            val no = BigInteger(1, messageDigest)
+
+            var hashtext = no.toString(16)
+            while (hashtext.length < 32) {
+                hashtext = "0$hashtext"
+            }
+            return hashtext
+        } catch (e: NoSuchAlgorithmException) {
+            throw RuntimeException(e)
+        }
+    }
+
+//    override fun getCharacter(callback: OnGetMarvelCallback) {
+//        val ts = System.currentTimeMillis().toString()
+//        var hash = getMd5(ts)
+//
+//        service.getCharacters(ts, SyncStateContract.Constants.Keys.PUBLIC_KEY, hash)
+//            .enqueue(object : Callback<ReturnData>{
+//                override fun onResponse(call: Call<ReturnData>, response: Response<ReturnData>) {
+//
+//                    if (response.isSuccessful){
+//                        if (response.body() != null){
+//                            heroesData.postValue(response.body())
+//                            callback.onSuccess(response.body()!!)
+//                        } else {
+//                            callback.onError()
+//                            Log.e("Response", " response null")
+//                        }
+//
+//                    } else {
+//                        callback.onError()
+//                        Log.e("Response", response.raw().networkResponse().toString())
+//                    }
+//
+//                }
+//
+//                override fun onFailure(call: Call<ReturnData>, t: Throwable) {
+//                    callback.onError()
+//                    t.printStackTrace()
+//                    Log.e("Response", javaClass.simpleName + " not response 2 " + t)
+//                }
+//            })
+//    }
 
 
 }
--- data/src/main/java/characters/CharacterApi.kt
+++ data/src/main/java/characters/CharacterApi.kt
@@ -2,14 +2,23 @@ package io.felipeandrade.marvelchars.data.characters
 
 import io.felipeandrade.marvelchars.domain.MarvelCharacter
 import retrofit2.http.GET
-import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface CharacterApi {
 
     @GET("/v1/public/characters")
-    suspend fun loadAllCharacters(): List<MarvelCharacter>
+    suspend fun loadAllCharacters(
+        @Query("ts") ts: String,
+        @Query("apikey") apiKey: String,
+        @Query("hash") hash: String
+    ): List<MarvelCharacter>
 
-    @GET("/v1/public/characters/{id}")
-    suspend fun loadCharacterById(@Path("id") charId: Int): MarvelCharacter
+    @GET("/v1/public/characters/")
+    suspend fun loadCharacterById(
+        @Query("ts") ts: String,
+        @Query("apikey") apiKey: String,
+        @Query("hash") hash: String,
+        @Query("id") charId: Int
+    ): MarvelCharacter
 
 }
